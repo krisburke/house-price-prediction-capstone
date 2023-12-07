7b76f50caff4917110551d49e444ccdc6ef9abf4
Kris Burke <kris.burke611@gmail.com>
2023-12-07 03:14:18 -0500

    Set up component library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Slider } from '@mantine/core';
+import { useHover } from '@mantine/hooks';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sliderValue, setSliderValue] = useState(30);
+  const { hovered, ref } = useHover();
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='card'>
+        <Slider
+          defaultValue={30}
+          value={sliderValue}
+          onChange={(value) => setSliderValue(value)}
+          ref={ref}
+          label={sliderValue}
+          styles={{
+            thumb: {
+              transition: 'opacity 150ms ease',
+              opacity: hovered ? 1 : 0,
+            },
+          }}
+        />
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import '@mantine/core/styles.css';
+// import '@mantine/form/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider defaultColorScheme='dark'>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
